d59dfbff59baa357f2674309ea1106fd9e13b608
Alex Naraghi <alexandernaraghi@gmail.com>
2015-07-30 20:02:22 -0700

    Some quick fixes to get the android project running.

---
+++ Client/Crosschat.Client.Android/Infrastructure/SquawkTransportResource.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using SharedSquawk.Client.Model.Contracts;
+using SharedSquawk.Server.Infrastructure;
+using Xamarin.Forms;
+using System.Net;
+using System.IO;
+using SharedSquawk.Server.Application.DataTransferObjects.Requests;
+using SharedSquawk.Client.Droid.Infrastructure;
+
+[assembly: Dependency(typeof(SquawkTransportResource))]
+
+namespace SharedSquawk.Client.Droid.Infrastructure
+{
+    public class 
+		SquawkTransportResource: ITransportResource
+    {
+        private bool _isConnected;
+        private const int BufferSize = 8 * 1024;
+        private bool _triedConnect = false;
+
+        public event Action ConnectionError = delegate { };
+		public event Action<ResponseBase> DataReceived = delegate { };
+        public event Action ConnectionStateChanged = delegate { };
+		private DtoSerializer _serializer;
+
+		public SquawkTransportResource()
+		{
+			_serializer = new DtoSerializer ();	
+		}
+        
+        public async Task ConnectAsync()
+        {
+            IsConnected = true;
+        }
+
+		public bool IsConnected
+		{
+			//ALEXTEST
+			get { return true;
+				//return _isConnected; 
+			}
+			private set
+			{
+				if (_isConnected != value)
+				{
+					ConnectionStateChanged();
+				}
+				_isConnected = value;
+			}
+		}
+
+        public async void DropConnection()
+        {
+            ConnectionError();
+        }
+
+        private void StartListening()
+        {
+        }
+
+        public Task DisconnectAsync()
+        {
+            return Task.FromResult(false);
+        }
+
+		public void SendData<TRequest, TResponse>(TransportEndpoint endpoint, TRequest data, long token) 
+			where TRequest : RequestBase 
+			where TResponse : ResponseBase
+		{
+			var request = (HttpWebRequest)WebRequest.Create(GlobalConfig.ServiceBase+endpoint.Address);
+
+			request.Method = endpoint.TransportMethod.ToString();
+
+			request.ContentType = "application/x-www-form-urlencoded";
+			request.UserAgent = GlobalConfig.UserAgent;
+			request.Referer = GlobalConfig.Referer;
+
+			request.Headers.Add ("X-Requested-With", GlobalConfig.RequestedWith);
+			request.Headers.Add (HttpRequestHeader.AcceptEncoding, GlobalConfig.AcceptEncoding);
+			//ALEXTEST
+			//TODO: Set the language
+			request.Headers.Add (HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.8");
+			request.Timeout = GlobalConfig.TimeoutMs;
+
+			Stream stream = request.GetRequestStream();
+			_serializer.Serialize<TRequest> (data, stream);
+			stream.Close();
+
+			try
+			{
+				request.BeginGetResponse(new AsyncCallback(OnDataReceived<TResponse>), new TokenRequest {Request = request, Token = token});
+			}
+			catch (Exception exc)
+			{
+				DropConnection();
+			}
+		}
+		private void OnDataReceived<T>(IAsyncResult ar) where T : ResponseBase
+		{
+			HttpWebRequest request;
+			HttpWebResponse response;
+			long token = 0;
+			ResponseBase responseObject;
+			try
+			{
+				var tokenRequest = ar.AsyncState as TokenRequest;
+				request = tokenRequest.Request;
+				token = tokenRequest.Token;
+				response = request.EndGetResponse (ar) as HttpWebResponse;
+				responseObject = _serializer.Deserialize<T> (response.GetResponseStream());
+			}
+			catch(Exception ex)
+			{
+				responseObject = Activator.CreateInstance<ResponseBase> ();
+				responseObject.Error = CommonErrors.Unknown;
+			}
+
+			//Here we are downcasting T to an object, but the token will allow us to figure out the type
+			responseObject.Token = token;
+			DataReceived(responseObject);
+		}
+
+    }
+
+
+	public class TokenRequest
+	{
+		public HttpWebRequest Request{ get; set;}
+		public long Token{ get; set;}
+	}
+}
